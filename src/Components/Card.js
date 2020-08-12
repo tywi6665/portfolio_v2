07f3a884370b7e445294e84101b7a076bc9a0576
tywi6665 <tyletcole@gmail.com>
2020-08-12 16:57:29 -0700

    Created screen and keyboard divs

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -4,7 +4,10 @@ const Card = ({ info }) => {
     return (
         <div className="card">
             <div className="card-inner">
-                <div className="card-screen"></div>
+                <div className="card-screen">
+                    <div id="screen"></div>
+                    <div id="keyboard"></div>
+                </div>
                 <div className="card-info"></div>
             </div>
         </div>
