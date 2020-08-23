2ea6573b398868a51c3a5c74648cecacc7ed189d
tywi6665 <tyletcole@gmail.com>
2020-08-23 13:47:36 -0600

    Created browser window for mp4s using css

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -7,9 +7,17 @@ const Card = ({ title, subTitle, description, icons, animation, links }) => {
             <div className="card-inner">
                 <div className="card-screen">
                     <div className="screen">
-                        <video autoplay="true" loop>
-                            <source src={`./videos/${animation}.mp4`} type="video/mp4" />
-                        </video>
+                        <div className="browser">
+                            <div className="browser-top">
+                                <span className="red"></span>
+                                <span className="yellow"></span>
+                                <span className="green"></span>
+                            </div>
+                            <video autoplay="true" loop>
+                                <source src={`./videos/${animation}.mp4`} type="video/mp4" />
+                            </video>
+                            <div className="browser-bottom"></div>
+                        </div>
                     </div>
                     <div className="keyboard"></div>
                 </div>
