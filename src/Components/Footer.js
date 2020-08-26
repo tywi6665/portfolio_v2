52c7d2b3af3f43cd889059166f410479a402b3d2
tywi6665 <tyletcole@gmail.com>
2020-08-25 18:58:56 -0600

    Beginning to style Footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,10 +2,11 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <>
-            <div>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></div>
-            <div>The wave background is provided by loading.io</div>
-        </>
+        <div className="footer">
+            <h4>Made by tywi6665</h4>
+            <p>Icons made by <a href="https://www.flaticon.com/authors/iconixar" title="iconixar">iconixar</a> from <a href="https://www.flaticon.com/" title="Flaticon">www.flaticon.com</a></p>
+            <p>The wave background is provided by loading.io</p>
+        </div>
     );
 }
 
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -4,30 +4,42 @@ import { Link, animateScroll as scroll } from "react-scroll";
 const Navbar = () => {
     return (
         <nav className="navbar">
-            <Link
-                activeClass="active"
-                to="projects-section"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-            >Projects</Link>
-            <Link
-                activeClass="active"
-                to="resume-section"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-            >Resume</Link>
-            <Link
-                activeClass="active"
-                to="contact-section"
-                spy={true}
-                smooth={true}
-                offset={-100}
-                duration={500}
-            >Contact</Link>
+            <div>
+                <Link
+                    activeClass="active"
+                    to="name-section"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Tyler Winstead</Link>
+            </div>
+            <div>
+                <Link
+                    activeClass="active"
+                    to="projects-section"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Projects</Link>
+                <Link
+                    activeClass="active"
+                    to="resume-section"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Resume</Link>
+                <Link
+                    activeClass="active"
+                    to="contact-section"
+                    spy={true}
+                    smooth={true}
+                    offset={-100}
+                    duration={500}
+                >Contact</Link>
+            </div>
         </nav>
     );
 }
