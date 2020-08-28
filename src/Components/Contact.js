f3c26728bc5cb724ffb59075b3402994369e495b
tywi6665 <tyletcole@gmail.com>
2020-08-27 19:52:29 -0600

    starting to style contact section

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -3,13 +3,23 @@ import React from 'react';
 const Contact = () => {
     return (
         <div className="contact">
-            <div className="contact-image">
+            <div className="contact-front">
+                <div className="contact-name">
+                    <p>TYLER WINSTEAD</p>
+                    <p>WEB DEVELOPER</p>
+                </div>
+                <span></span>
+                <div className="contact-initials">
+                    <p>TW</p>
+                </div>
+            </div>
+            {/* <div className="contact-image">
                 <img src="./graphics/me.jpg" alt="me" />
             </div>
             <div className="contact-info">
                 <p><a href="mailto:tyletcole@gmail.com?subject=I%20Liked%20Your%20Portfolio%20and%20Wanted%20to%20Reach%20Out" target="_blank" rel="noopener noreferrer">Email Me!!</a></p>
                 <p><a href="https://github.com/tywi6665" target="_blank" rel="noopener noreferrer">My GitHub</a></p>
-            </div>
+            </div> */}
         </div>
     );
 }
