b809520a07e575fa16e423e623fbda7e3552d163
tywi6665 <tyletcole@gmail.com>
2020-09-12 15:24:41 -0600

    Updates

---
+++ precache-manifest.4ad63332b94a01cce8cb8503b45f1cdc.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a4d1d0790a078948c57d351d56cd2a1",
+    "url": "/porfolio_v2/7a828ef684b970aabf06.worker.js"
+  },
+  {
+    "revision": "69cd844e5198ac97a0aad1207ced67af",
+    "url": "/porfolio_v2/7a828ef684b970aabf06.worker.js.LICENSE.txt"
+  },
+  {
+    "revision": "2750da127c08d36830802cb265ca92eb",
+    "url": "/porfolio_v2/index.html"
+  },
+  {
+    "revision": "eeb7a6a53ebe2c82e6af",
+    "url": "/porfolio_v2/static/css/main.f62961de.chunk.css"
+  },
+  {
+    "revision": "89c02892e6e2ebe15ad4",
+    "url": "/porfolio_v2/static/js/2.f85e6757.chunk.js"
+  },
+  {
+    "revision": "908678959397f2e04ac5f8f7afda467f",
+    "url": "/porfolio_v2/static/js/2.f85e6757.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eeb7a6a53ebe2c82e6af",
+    "url": "/porfolio_v2/static/js/main.50822088.chunk.js"
+  },
+  {
+    "revision": "1de9edcf1a71a6ccf6f6",
+    "url": "/porfolio_v2/static/js/runtime-main.a28df02d.js"
+  },
+  {
+    "revision": "03135c5660f2e3a990e1a998ca6bf2e6",
+    "url": "/porfolio_v2/static/media/Tyler_Winstead_Resume_Redacted.03135c56.pdf"
+  },
+  {
+    "revision": "a983fec6d0b50dc2adda61aea0f71854",
+    "url": "/porfolio_v2/static/media/Wave1.a983fec6.svg"
+  },
+  {
+    "revision": "0b1787878692ef6f5ab9f862fda0a056",
+    "url": "/porfolio_v2/static/media/Waves.0b178787.png"
+  }
+]);
