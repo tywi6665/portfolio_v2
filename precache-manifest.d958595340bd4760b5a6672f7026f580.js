5ae56930e48cb68178ea8af258d4573500e8a4e3
tywi6665 <34082648+tywi6665@users.noreply.github.com>
2020-09-20 11:07:07 -0600

    Update precache-manifest.d958595340bd4760b5a6672f7026f580.js

--- precache-manifest.d958595340bd4760b5a6672f7026f580.js
+++ precache-manifest.d958595340bd4760b5a6672f7026f580.js
@@ -1,46 +1,46 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "fb555ef25a52c4afedf0312f42250fbb",
-    "url": "/portfolio_v2/16e534c0d6b045d9afc5.worker.js"
+    "url": "/16e534c0d6b045d9afc5.worker.js"
   },
   {
     "revision": "69cd844e5198ac97a0aad1207ced67af",
-    "url": "/portfolio_v2/16e534c0d6b045d9afc5.worker.js.LICENSE.txt"
+    "url": "/16e534c0d6b045d9afc5.worker.js.LICENSE.txt"
   },
   {
     "revision": "17a53e65fb6a15d979da65d2e34278b3",
-    "url": "/portfolio_v2/index.html"
+    "url": "/index.html"
   },
   {
     "revision": "292051734c671f84f4f8",
-    "url": "/portfolio_v2/static/css/main.b5ae3398.chunk.css"
+    "url": "/static/css/main.b5ae3398.chunk.css"
   },
   {
     "revision": "b47faea0ccc2b8bb21fd",
-    "url": "/portfolio_v2/static/js/2.67e6da31.chunk.js"
+    "url": "/static/js/2.67e6da31.chunk.js"
   },
   {
     "revision": "908678959397f2e04ac5f8f7afda467f",
-    "url": "/portfolio_v2/static/js/2.67e6da31.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.67e6da31.chunk.js.LICENSE.txt"
   },
   {
     "revision": "292051734c671f84f4f8",
-    "url": "/portfolio_v2/static/js/main.df6fda8b.chunk.js"
+    "url": "/static/js/main.df6fda8b.chunk.js"
   },
   {
     "revision": "50e364d9cf87c6b57bee",
-    "url": "/portfolio_v2/static/js/runtime-main.2914acf5.js"
+    "url": "/static/js/runtime-main.2914acf5.js"
   },
   {
     "revision": "03135c5660f2e3a990e1a998ca6bf2e6",
-    "url": "/portfolio_v2/static/media/Tyler_Winstead_Resume_Redacted.03135c56.pdf"
+    "url": "/static/media/Tyler_Winstead_Resume_Redacted.03135c56.pdf"
   },
   {
     "revision": "a983fec6d0b50dc2adda61aea0f71854",
-    "url": "/portfolio_v2/static/media/Wave1.a983fec6.svg"
+    "url": "/static/media/Wave1.a983fec6.svg"
   },
   {
     "revision": "0b1787878692ef6f5ab9f862fda0a056",
-    "url": "/portfolio_v2/static/media/Waves.0b178787.png"
+    "url": "/static/media/Waves.0b178787.png"
   }
 ]);
